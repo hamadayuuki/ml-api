43dd2a1fc4171f8ab4c526a371eaf8d3a01fcb56
hamadayuuki <yuukirinnma@gmail.com>
2023-07-23 12:12:10 +0900

    update: yolov5モデルを生成する箇所を変更

--- yolov5/yolov5.py
+++ yolov5/yolov5.py
@@ -4,23 +4,21 @@ import io
 from PIL import Image
 import time
 
-class Model:
-    def __init__(self):
-        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+def model(saveDir, imageURL):
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+    results = model(imageURL)
+    results.save(save_dir = saveDir)
 
-    def model(self, saveDir, imageURL):
-        results = self.model(imageURL)
-        results.save(save_dir = saveDir)
+    return results
 
-        return results
+def model_render(saveDir, imageURL):
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+    results = model(imageURL)
 
-    def model_render(self, saveDir, imageURL):
-        results = self.model(imageURL)
+    # image to base64
+    buffered = io.BytesIO()
+    img_base64 = Image.fromarray(results.ims[0])
+    img_base64.save(buffered, format="JPEG")
+    print(base64.b64encode(buffered.getvalue()).decode('utf-8')) 
 
-        # image to base64
-        buffered = io.BytesIO()
-        img_base64 = Image.fromarray(results.ims[0])
-        img_base64.save(buffered, format="JPEG")
-        print(base64.b64encode(buffered.getvalue()).decode('utf-8')) 
-
-        return base64.b64encode(buffered.getvalue()).decode('utf-8')
+    return base64.b64encode(buffered.getvalue()).decode('utf-8')
